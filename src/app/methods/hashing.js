df41fcc3d2f73d5f7dbb3d9a439def391d9a4f34
reymom <ramon_gs95@hotmail.com>
2023-09-10 17:03:42 +0200

    fixes

--- src/app/methods/hashing.js
+++ src/app/methods/hashing.js
@@ -1,64 +1,62 @@
-import { MerkleTree, Hasher } from "./merkleTree";
-import { BigNumber, BigNumberish } from "ethers";
-import { poseidonContract, buildPoseidon } from "circomlibjs";
-import { ethers } from "hardhat";
+import { Hasher } from "./merkleTree"
+import { BigNumber } from "ethers"
+import { poseidonContract } from "circomlibjs"
+import { ethers } from "hardhat"
 
 function poseidonHash(poseidon, inputs) {
-    const hash = poseidon(inputs.map(x => BigNumber.from(x).toBigInt()));
-    // Make the number within the field size
-    const hashStr = poseidon.F.toString(hash);
-    // Make it a valid hex string
-    const hashHex = BigNumber.from(hashStr).toHexString();
-    // pad zero to make it 32 bytes, so that the output can be taken as a bytes32 contract argument
-    const bytes32 = ethers.utils.hexZeroPad(hashHex, 32);
-    return bytes32;
+  const hash = poseidon(inputs.map(x => BigNumber.from(x).toBigInt()))
+  // Make the number within the field size
+  const hashStr = poseidon.F.toString(hash)
+  // Make it a valid hex string
+  const hashHex = BigNumber.from(hashStr).toHexString()
+  // pad zero to make it 32 bytes, so that the output can be taken as a bytes32 contract argument
+  const bytes32 = ethers.utils.hexZeroPad(hashHex, 32)
+  return bytes32
 }
 
 class PoseidonHasher extends Hasher {
-    constructor(poseidon) {
-        this.poseidon = poseidon;
-    }
+  constructor(poseidon) {
+    this.poseidon = poseidon
+  }
 
-    hash(left, right) {
-        return poseidonHash(this.poseidon, [left, right]);
-    }
+  hash(left, right) {
+    return poseidonHash(this.poseidon, [left, right])
+  }
 }
-
 
 class Deposit {
-    private constructor(
-        public readonly nullifier: Uint8Array,
-        public poseidon: any,
-        public leafIndex?: number
-    ) {
-        this.poseidon = poseidon;
-    }
-    static new(poseidon: any) {
-        // random nullifier (private note)
-        // here we only have private nullifier 
-        const nullifier = ethers.utils.randomBytes(15);
-        return new this(nullifier, poseidon);
-    }
-    // get hash of secret (nullifier)
-    get commitment() {
-        return poseidonHash(this.poseidon, [this.nullifier, 0]);
-    }
-    // get hash f nullifierhash (nulifier+1+index)
-    get nullifierHash() {
-        if (!this.leafIndex && this.leafIndex !== 0)
-            throw Error("leafIndex is unset yet");
-        return poseidonHash(this.poseidon, [this.nullifier, 1, this.leafIndex]);
-    }
+  constructor(nullifier, poseidon, leafIndex) {
+    this.nullifier = nullifier
+    this.poseidon = poseidon
+    this.leafIndex = leafIndex
+    this.poseidon = poseidon
+  }
+  static new(poseidon) {
+    // random nullifier (private note)
+    // here we only have private nullifier
+    const nullifier = ethers.utils.randomBytes(15)
+    return new this(nullifier, poseidon)
+  }
+  // get hash of secret (nullifier)
+  get commitment() {
+    return poseidonHash(this.poseidon, [this.nullifier, 0])
+  }
+  // get hash f nullifierhash (nulifier+1+index)
+  get nullifierHash() {
+    if (!this.leafIndex && this.leafIndex !== 0)
+      throw Error("leafIndex is unset yet")
+    return poseidonHash(this.poseidon, [this.nullifier, 1, this.leafIndex])
+  }
 }
 
-function getPoseidonFactory(nInputs: number) {
-    const bytecode = poseidonContract.createCode(nInputs);
-    const abiJson = poseidonContract.generateABI(nInputs);
-    const abi = new ethers.utils.Interface(abiJson);
-    return new ContractFactory(abi, bytecode);
+function getPoseidonFactory(nInputs) {
+  const bytecode = poseidonContract.createCode(nInputs)
+  const abiJson = poseidonContract.generateABI(nInputs)
+  const abi = new ethers.utils.Interface(abiJson)
+  return new ContractFactory(abi, bytecode)
 }
 
-await function deposit(amount: number) {
+async function deposit(amount) {
     poseidon = await buildPoseidon();
     const [userOldSigner, relayerSigner, userNewSigner] = await ethers.getSigners();
     const deposit = Deposit.new(poseidon);
