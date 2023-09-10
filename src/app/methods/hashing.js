9046bd4040083ef3053b4570743fe70b99882b5d
reymom <ramon_gs95@hotmail.com>
2023-09-10 15:35:04 +0200

    merkle tree and localstorage

--- src/app/methods/hashing.js
+++ src/app/methods/hashing.js
@@ -1,2 +1,27 @@
-// list of hasing methods here
+import { MerkleTree, Hasher } from "./merkleTree";
+import { BigNumber, BigNumberish } from "ethers";
+import { ethers } from "hardhat";
+
+function poseidonHash(poseidon: any, inputs: BigNumberish[]): string {
+    const hash = poseidon(inputs.map((x) => BigNumber.from(x).toBigInt()));
+    // Make the number within the field size
+    const hashStr = poseidon.F.toString(hash);
+    // Make it a valid hex string
+    const hashHex = BigNumber.from(hashStr).toHexString();
+    // pad zero to make it 32 bytes, so that the output can be taken as a bytes32 contract argument
+    const bytes32 = ethers.utils.hexZeroPad(hashHex, 32);
+    return bytes32;
+}
+
+class PoseidonHasher implements Hasher {
+    poseidon: any;
+
+    constructor(poseidon: any) {
+        this.poseidon = poseidon;
+    }
+
+    hash(left: string, right: string) {
+        return poseidonHash(this.poseidon, [left, right]);
+    }
+}
 
